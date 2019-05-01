66f06cd8654ea56d17d08e6f92b5365dde0cf0c3
Casey Macaulay <cmacaulay@terakeet.com>
2019-05-01 17:33:31 -0400

    add old implementation back in, add benchmark to measure performance

--- search_engine.rb
+++ search_engine.rb
@@ -1,3 +1,11 @@
+# documents, are strings
+# index the document - give the document an idea
+# search a given word and the result is the IDs where the word appears
+# Create a data structure: index, accepts a string and the ID
+# String is the body of the document, ID identifies the document
+# SEARCH - provide a word, another string, get returned all of the ids where that word appears
+require 'benchmark'
+
 class SearchEngine
   attr_reader :index
 
@@ -6,6 +14,26 @@ class SearchEngine
   end
 
   def add(document, id)
+    # Hash, take the ID and store that as the key
+    # Take the document and store that as the value
+    # doesn't return anything
+    @index[id] = document
+  end
+
+  def search(word)
+    result = []
+    @index.each do |id, value|
+      if value.include?(word)
+        result << id
+      end
+    end
+    result
+    # Iterate over that hash structure, look at both the key and the value
+    # search the value (document), for the word
+    # Returns an array of the ids
+  end
+
+  def add_inverse_index(document, id)
     document.split(" ").each do |word|
       word = word.downcase
       if !@index[word]
@@ -16,14 +44,38 @@ class SearchEngine
     end
   end
 
-  def search(word)
+  def search_inverse_index(word)
     @index[word.downcase]
   end
 end
 
+test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id nibh tortor id aliquet lectus proin. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Viverra mauris in aliquam sem fringilla ut morbi. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Cursus metus aliquam eleifend mi in nulla. Ornare lectus sit amet est placerat in egestas erat. Id leo in vitae turpis massa sed elementum tempus egestas. Luctus accumsan tortor posuere ac ut consequat semper. Egestas tellus rutrum tellus pellentesque eu. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Purus ut faucibus pulvinar elementum integer enim neque. Adipiscing commodo elit at imperdiet dui accumsan sit. Viverra accumsan in nisl nisi scelerisque eu. Iaculis nunc sed augue lacus viverra. Vel facilisis volutpat est velit egestas. Dolor sed viverra ipsum nunc aliquet bibendum enim. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Vestibulum morbi blandit cursus risus at. Risus nullam eget felis eget nunc lobortis mattis aliquam faucibus. Aliquam sem et tortor consequat id porta nibh venenatis cras. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Orci nulla pellentesque dignissim enim. Quam pellentesque nec nam aliquam sem et tortor consequat id. Risus at ultrices mi tempus. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Platea dictumst vestibulum rhoncus est. Dictum fusce ut placerat orci nulla pellentesque. Fusce id velit ut tortor. Quam quisque id diam vel quam elementum pulvinar. Maecenas volutpat blandit aliquam etiam erat. Amet massa vitae tortor condimentum lacinia quis vel eros.Ultrices mi tempus imperdiet nulla. Id faucibus nisl tincidunt eget. Ligula ullamcorper malesuada proin libero nunc. Pellentesque id nibh tortor id aliquet. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Tortor posuere ac ut consequat. Mattis pellentesque id nibh tortor id. Iaculis eu non diam phasellus vestibulum. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Faucibus purus in massa tempor nec feugiat nisl. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Vitae justo eget magna fermentum iaculis. Maecenas ultricies mi eget mauris. Ullamcorper a lacus vestibulum sed arcu non."
+
 s = SearchEngine.new()
 s.add("This is the test.", 1)
 s.add("Copenhagen in the Spring.", 15)
+s.add(test_string, 3)
+
 p s.search("the")
 p s.search("Munich")
 p s.search("Copenhagen")
+p s.search("non")
+
+s.add_inverse_index("This is the test.", 1)
+s.add_inverse_index("Copenhagen in the Spring.", 15)
+s.add_inverse_index(test_string, 3)
+p s.search_inverse_index("the")
+p s.search_inverse_index("Munich")
+p s.search_inverse_index("Copenhagen")
+p s.search_inverse_index("non")
+
+Benchmark.bm do |x|
+  x.report {s.search("the")}
+  x.report {s.search_inverse_index("the")}
+  x.report {s.search("Munich")}
+  x.report {s.search_inverse_index("Munich")}
+  x.report {s.search("Copenhagen")}
+  x.report {s.search_inverse_index("Copenhagen")}
+  x.report {s.search("non")}
+  x.report {s.search_inverse_index("non")}
+end
