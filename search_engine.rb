f17dad74e3b03b1ce532e9def1c54b059c74b894
Casey Macaulay <cmacaulay@terakeet.com>
2019-05-01 23:27:38 -0400

    add test for inverse index implementation

--- search_engine.rb
+++ search_engine.rb
@@ -42,12 +42,8 @@ end
 
 test_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id nibh tortor id aliquet lectus proin. Purus ut faucibus pulvinar elementum integer enim neque volutpat. Viverra mauris in aliquam sem fringilla ut morbi. Nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Cursus metus aliquam eleifend mi in nulla. Ornare lectus sit amet est placerat in egestas erat. Id leo in vitae turpis massa sed elementum tempus egestas. Luctus accumsan tortor posuere ac ut consequat semper. Egestas tellus rutrum tellus pellentesque eu. Proin fermentum leo vel orci porta non pulvinar neque laoreet. Ac turpis egestas maecenas pharetra convallis posuere morbi leo urna. Cras fermentum odio eu feugiat pretium nibh ipsum consequat nisl. Purus ut faucibus pulvinar elementum integer enim neque. Adipiscing commodo elit at imperdiet dui accumsan sit. Viverra accumsan in nisl nisi scelerisque eu. Iaculis nunc sed augue lacus viverra. Vel facilisis volutpat est velit egestas. Dolor sed viverra ipsum nunc aliquet bibendum enim. Nunc scelerisque viverra mauris in aliquam sem fringilla ut. Vestibulum morbi blandit cursus risus at. Risus nullam eget felis eget nunc lobortis mattis aliquam faucibus. Aliquam sem et tortor consequat id porta nibh venenatis cras. Luctus venenatis lectus magna fringilla urna porttitor rhoncus. Orci nulla pellentesque dignissim enim. Quam pellentesque nec nam aliquam sem et tortor consequat id. Risus at ultrices mi tempus. Nunc pulvinar sapien et ligula ullamcorper malesuada proin. Platea dictumst vestibulum rhoncus est. Dictum fusce ut placerat orci nulla pellentesque. Fusce id velit ut tortor. Quam quisque id diam vel quam elementum pulvinar. Maecenas volutpat blandit aliquam etiam erat. Amet massa vitae tortor condimentum lacinia quis vel eros.Ultrices mi tempus imperdiet nulla. Id faucibus nisl tincidunt eget. Ligula ullamcorper malesuada proin libero nunc. Pellentesque id nibh tortor id aliquet. Nunc mi ipsum faucibus vitae aliquet nec ullamcorper sit amet. Tortor posuere ac ut consequat. Mattis pellentesque id nibh tortor id. Iaculis eu non diam phasellus vestibulum. Molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Faucibus purus in massa tempor nec feugiat nisl. Curabitur gravida arcu ac tortor dignissim convallis aenean et tortor. Molestie a iaculis at erat pellentesque adipiscing commodo elit. Vitae justo eget magna fermentum iaculis. Maecenas ultricies mi eget mauris. Ullamcorper a lacus vestibulum sed arcu non."
 
-s = SearchEngine.new()
-s.add("This is the test.", 0)
-s.add("Copenhagen in the Spring.", 1)
 
-
-p s.search("the")
+s = SearchEngine.new
 p s.search("Munich")
 p s.search("Copenhagen")
 p s.search("non")
--- search_engine_test.rb
+++ search_engine_test.rb
@@ -2,8 +2,33 @@ require 'minitest/autorun'
 require_relative 'search_engine'
 class SearchEngineTest < Minitest::Test
   def test_search_engine_initializes
-    search_engine = SearchEngine.new()
+    search_engine = SearchEngine.new
+
     assert_equal SearchEngine, search_engine.class
     assert_equal Hash.new, search_engine.index
+  end
+
+  def test_index_implementation_will_add_and_search
+    search_engine = SearchEngine.new
+
+    search_engine.add("This is the first test.", 0)
+    index = search_engine.index
+
+    assert_equal "This is the first test.", index[0]
+    assert_equal [0], search_engine.search("the")
+  end
+
+  def test_index_will_add_and_search
+    search_engine = SearchEngine.new
+    # note: edge case, punctuation needs to be stripped
+    search_engine.add_inverse_index("Copenhagen in the Spring", 3)
+    index = search_engine.index
+
+    assert_equal [3], index["copenhagen"]
+    assert_equal [3], index["spring"]
+
+    search_engine.add_inverse_index("This is the second test.", 0)
+
+    assert_equal [3, 0], search_engine.search_inverse_index("the")
   end
 end
